192192619f2ee11d2fdba8c58a3489c338e0ff42
Joydeep Mukherjee <joydeep1701@gmail.com>
2017-08-12 20:25:11 +0530

    Print GSTR1

--- app.py
+++ app.py
@@ -575,6 +575,14 @@ def gstr1_b2b(m):
 
     state_codes = {"35":"35-Andaman and Nicobar Islands","37":"37-Andhra Pradesh","12":"12-Arunachal Pradesh","18":"18-Assam","10":"10-Bihar","04":"04-Chandigarh","22":"22-Chhattisgarh","26":"26-Dadra and Nagar Haveli","25":"25-Daman and Diu","07":"07-Delhi","30":"30-Goa","24":"24-Gujarat","06":"06-Haryana","02":"02-Himachal Pradesh","01":"01-Jammu and Kashmir","20":"20-Jharkhand","29":"29-Karnataka","32":"32-Kerala","31":"31-Lakshadweep","23":"23-Madhya Pradesh","27":"27-Maharashtra","14":"14-Manipur","17":"17-Meghalaya","15":"15-Mizoram","13":"13-Nagaland","21":"21-Odisha","34":"34-Puducherry","03":"03-Punjab","08":"08-Rajasthan","11":"11-Sikkim","33":"33-Tamil Nadu","36":"36-Telangana","16":"16-Tripura","09":"09-Uttar Pradesh","05":"05-Uttarakhand","97":"97-Other Territory","19":"19-West Bengal"}
     return render_template("gstr1_b2b.html",data=rows,month=m,state_codes=state_codes)
+@app.route('/gstr1/b2b/<m>/print')
+def print_gstr1_b2b(m):
+    b2b = str(session["company_id"])+"_gstr1_sales"
+    ur_id = get_ledger_id("Unregistered Sale")
+    rows = db.execute("SELECT * FROM :table WHERE month=:month AND debtor_id != :ur_id",table=b2b,month=m,ur_id=ur_id)
+
+    state_codes = {"35":"35-Andaman and Nicobar Islands","37":"37-Andhra Pradesh","12":"12-Arunachal Pradesh","18":"18-Assam","10":"10-Bihar","04":"04-Chandigarh","22":"22-Chhattisgarh","26":"26-Dadra and Nagar Haveli","25":"25-Daman and Diu","07":"07-Delhi","30":"30-Goa","24":"24-Gujarat","06":"06-Haryana","02":"02-Himachal Pradesh","01":"01-Jammu and Kashmir","20":"20-Jharkhand","29":"29-Karnataka","32":"32-Kerala","31":"31-Lakshadweep","23":"23-Madhya Pradesh","27":"27-Maharashtra","14":"14-Manipur","17":"17-Meghalaya","15":"15-Mizoram","13":"13-Nagaland","21":"21-Odisha","34":"34-Puducherry","03":"03-Punjab","08":"08-Rajasthan","11":"11-Sikkim","33":"33-Tamil Nadu","36":"36-Telangana","16":"16-Tripura","09":"09-Uttar Pradesh","05":"05-Uttarakhand","97":"97-Other Territory","19":"19-West Bengal"}
+    return render_template("gstr1_b2b_print.html",data=rows,month=m,state_codes=state_codes)
 @app.route('/gstr1/b2cl/<m>')
 def gstr1_b2cl(m):
     b2cl = str(session["company_id"])+"_gstr1_sales"
@@ -586,6 +594,17 @@ def gstr1_b2cl(m):
 
     state_codes = {"35":"35-Andaman and Nicobar Islands","37":"37-Andhra Pradesh","12":"12-Arunachal Pradesh","18":"18-Assam","10":"10-Bihar","04":"04-Chandigarh","22":"22-Chhattisgarh","26":"26-Dadra and Nagar Haveli","25":"25-Daman and Diu","07":"07-Delhi","30":"30-Goa","24":"24-Gujarat","06":"06-Haryana","02":"02-Himachal Pradesh","01":"01-Jammu and Kashmir","20":"20-Jharkhand","29":"29-Karnataka","32":"32-Kerala","31":"31-Lakshadweep","23":"23-Madhya Pradesh","27":"27-Maharashtra","14":"14-Manipur","17":"17-Meghalaya","15":"15-Mizoram","13":"13-Nagaland","21":"21-Odisha","34":"34-Puducherry","03":"03-Punjab","08":"08-Rajasthan","11":"11-Sikkim","33":"33-Tamil Nadu","36":"36-Telangana","16":"16-Tripura","09":"09-Uttar Pradesh","05":"05-Uttarakhand","97":"97-Other Territory","19":"19-West Bengal"}
     return render_template("gstr1_b2cl.html",data=rows,month=m,state_codes=state_codes)
+@app.route('/gstr1/b2cl/<m>/print')
+def print_gstr1_b2cl(m):
+    b2cl = str(session["company_id"])+"_gstr1_sales"
+    ur_id = get_ledger_id("Unregistered Sale")
+    home_state = '19'
+    rows = db.execute("""SELECT * FROM :table WHERE month=:month
+    AND debtor_id = :ur_id AND invoice_value > 250000 AND place_of_supply!=:hs""",
+    table=b2cl,month=m,ur_id=ur_id,hs=home_state)
+
+    state_codes = {"35":"35-Andaman and Nicobar Islands","37":"37-Andhra Pradesh","12":"12-Arunachal Pradesh","18":"18-Assam","10":"10-Bihar","04":"04-Chandigarh","22":"22-Chhattisgarh","26":"26-Dadra and Nagar Haveli","25":"25-Daman and Diu","07":"07-Delhi","30":"30-Goa","24":"24-Gujarat","06":"06-Haryana","02":"02-Himachal Pradesh","01":"01-Jammu and Kashmir","20":"20-Jharkhand","29":"29-Karnataka","32":"32-Kerala","31":"31-Lakshadweep","23":"23-Madhya Pradesh","27":"27-Maharashtra","14":"14-Manipur","17":"17-Meghalaya","15":"15-Mizoram","13":"13-Nagaland","21":"21-Odisha","34":"34-Puducherry","03":"03-Punjab","08":"08-Rajasthan","11":"11-Sikkim","33":"33-Tamil Nadu","36":"36-Telangana","16":"16-Tripura","09":"09-Uttar Pradesh","05":"05-Uttarakhand","97":"97-Other Territory","19":"19-West Bengal"}
+    return render_template("gstr1_b2cl_print.html",data=rows,month=m,state_codes=state_codes)
 @app.route('/gstr1/b2cs/<m>')
 def gstr1_b2cs(m):
     b2cs = str(session["company_id"])+"_gstr1_sales"
@@ -598,6 +617,18 @@ def gstr1_b2cs(m):
     """,table=b2cs,month=m,ur_id=ur_id,hs=home_state)
     state_codes = {"35":"35-Andaman and Nicobar Islands","37":"37-Andhra Pradesh","12":"12-Arunachal Pradesh","18":"18-Assam","10":"10-Bihar","04":"04-Chandigarh","22":"22-Chhattisgarh","26":"26-Dadra and Nagar Haveli","25":"25-Daman and Diu","07":"07-Delhi","30":"30-Goa","24":"24-Gujarat","06":"06-Haryana","02":"02-Himachal Pradesh","01":"01-Jammu and Kashmir","20":"20-Jharkhand","29":"29-Karnataka","32":"32-Kerala","31":"31-Lakshadweep","23":"23-Madhya Pradesh","27":"27-Maharashtra","14":"14-Manipur","17":"17-Meghalaya","15":"15-Mizoram","13":"13-Nagaland","21":"21-Odisha","34":"34-Puducherry","03":"03-Punjab","08":"08-Rajasthan","11":"11-Sikkim","33":"33-Tamil Nadu","36":"36-Telangana","16":"16-Tripura","09":"09-Uttar Pradesh","05":"05-Uttarakhand","97":"97-Other Territory","19":"19-West Bengal"}
     return render_template("gstr1_b2cs.html",data=rows,month=m,state_codes=state_codes)
+@app.route('/gstr1/b2cs/<m>/print')
+def print_gstr1_b2cs(m):
+    b2cs = str(session["company_id"])+"_gstr1_sales"
+    home_state = '19'
+    ur_id = get_ledger_id("Unregistered Sale")
+    rows = db.execute("""SELECT * FROM :table WHERE month=:month
+    AND debtor_id = :ur_id AND
+    ((place_of_supply = :hs)
+      OR (place_of_supply != :hs AND invoice_value < 250001))
+    """,table=b2cs,month=m,ur_id=ur_id,hs=home_state)
+    state_codes = {"35":"35-Andaman and Nicobar Islands","37":"37-Andhra Pradesh","12":"12-Arunachal Pradesh","18":"18-Assam","10":"10-Bihar","04":"04-Chandigarh","22":"22-Chhattisgarh","26":"26-Dadra and Nagar Haveli","25":"25-Daman and Diu","07":"07-Delhi","30":"30-Goa","24":"24-Gujarat","06":"06-Haryana","02":"02-Himachal Pradesh","01":"01-Jammu and Kashmir","20":"20-Jharkhand","29":"29-Karnataka","32":"32-Kerala","31":"31-Lakshadweep","23":"23-Madhya Pradesh","27":"27-Maharashtra","14":"14-Manipur","17":"17-Meghalaya","15":"15-Mizoram","13":"13-Nagaland","21":"21-Odisha","34":"34-Puducherry","03":"03-Punjab","08":"08-Rajasthan","11":"11-Sikkim","33":"33-Tamil Nadu","36":"36-Telangana","16":"16-Tripura","09":"09-Uttar Pradesh","05":"05-Uttarakhand","97":"97-Other Territory","19":"19-West Bengal"}
+    return render_template("gstr1_b2cs_print.html",data=rows,month=m,state_codes=state_codes)
 @app.route('/gstr1/download/<t>/<m>')
 def gstr1_download(t,m):
     company = db.execute("SELECT * FROM master WHERE company_id=:id",id=session["company_id"])
